bc73ea04f264df83021de8f1642d522884382267
caiyingrong <yingrong.cai@qq.com>
2019-08-28 15:00:45 +0800

    fix python installer

--- python-3.6.9.centos7.sh
+++ python-3.6.9.centos7.sh
@@ -7,9 +7,16 @@ tar -zxvf Python-3.6.9.tgz
 rm Python-3.6.9.tgz
 cd Python-3.6.9
 ./configure --prefix=/usr/local/python369  --enable-optimizations
-make && make install
+sudo make && make install
 sudo ln -sf /usr/local/python369/bin/python3 /usr/bin/python3
 sudo ln -sf /usr/local/python369/bin/pip3 /usr/bin/pip3
+sudo ln -sf /usr/local/python369/bin/python3 /usr/local/bin/python3
+sudo ln -sf /usr/local/python369/bin/pip3 /usr/local/bin/pip3
+sudo ln -sf /usr/local/python369/bin/python3 /usr/bin/python3.6
+sudo ln -sf /usr/local/python369/bin/pip3 /usr/bin/pip3.6
+sudo ln -sf /usr/local/python369/bin/python3 /usr/local/bin/python3.6
+sudo ln -sf /usr/local/python369/bin/pip3 /usr/local/bin/pip3.6
+sudo python3 -m pip install pip --upgrade
 python3 --version
 pip3 --version
 
